21144a36a75fdf099767d25ecd07c9444b9551b9
Pawas44 <bilotakala28@gmail.com>
2025-06-13 12:46:26 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const banRoutes = require('./routes/baninfo')
+
+
+app.use('/', banRoutes)
+
+app.listen(5000, ()=>{
+    console.log('server running on http://localhost:5000');
+})
